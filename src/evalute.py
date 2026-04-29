2e3cb2722f571fa52c389940033efd21f14aec44
kayarkar007 <KAYARKARPAVANKALYANOFFICIAL@protonmail.com>
2026-04-29 20:05:55 +0530

    added prediction and imported model from churn_model.pkl for preduction

---
+++ src/evalute.py
@@ -0,0 +1,6 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+def evaluate(y_test, y_pred):
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("Precision:", precision_score(y_test, y_pred))
+    print("Recall:", recall_score(y_test, y_pred))
